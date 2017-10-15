95ccf62dcf76dc5ff4edfba366104534b42816d0
Regalado, Jonathan <Jonathan.Regalado7@T-Mobile.com>
2017-10-15 09:13:22 -0700

    Added base collapsing toolbar

--- app/src/main/java/com/gimmyo/app/bidboard/BidBoardHome.java
+++ app/src/main/java/com/gimmyo/app/bidboard/BidBoardHome.java
@@ -2,10 +2,14 @@ package com.gimmyo.app.bidboard;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 
 import com.gimmyo.app.R;
 import com.gimmyo.app.bidboard.adapter.BidItemAdapter;
@@ -39,6 +43,10 @@ public class BidBoardHome extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         binding.mainBidboard.setLayoutManager(layoutManager);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        floatingActionSetup();
+
         ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
 
@@ -62,5 +70,17 @@ public class BidBoardHome extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void floatingActionSetup() {
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
