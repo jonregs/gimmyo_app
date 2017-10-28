eb24f9e631961286fe9f6c93d202a3e2c25bae91
jonregalado <jonregalado@users.noreply.github.com>
2017-10-28 14:34:40 -0700

    working login and signup pages

--- app/src/main/java/com/gimmyo/app/MainActivity.java
+++ app/src/main/java/com/gimmyo/app/MainActivity.java
@@ -1,33 +1,41 @@
 package com.gimmyo.app;
 
+import android.app.Application;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-import com.gimmyo.app.bidboard.BidBoardHome;
+import com.gimmyo.app.login.MainLoginPage;
+import com.parse.Parse;
 
 /**
  * Created by Jon on 9/29/2017.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Application {
 
     String TAG = "BidBoard";
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Enable Local Data Store.
+        Parse.enableLocalDatastore(this);
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, BidBoardHome.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        /*
+         Parse Login Credentials
+         URL: http://ec2-18-220-7-92.us-east-2.compute.amazonaws.com/apps/My%20Bitnami%20Parse%20API/browser/_User
+         Login to Parse Server
+         username: user
+         password: j8yiiNOCeurK
+        */
+        
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("8812ae35cb75fe6d52f018ec0a8add5339b999e8")
+                .clientKey("dc0190dd5f7ca905b849a6ab18d292c4602dec93")
+                .server("http://ec2-18-220-7-92.us-east-2.compute.amazonaws.com:80/parse/")
+                .build()
+        );
+        
+        Intent startApp = new Intent(MainActivity.this, MainLoginPage.class);
+        startActivity(startApp);
     }
 }
